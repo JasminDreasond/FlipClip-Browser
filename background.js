8df6a33d2895c0ceb447a1c448a74db3965ef867
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-12-27 16:45:31 -0300

    preparing url validator

--- background.js
+++ background.js
@@ -1,14 +1,37 @@
-chrome.action.onClicked.addListener(function(tab) {
+// Open NFT Script
+const openNFTPage = function(tabID, vanillaURL, newTab) {
+
     /*     chrome.windows.create({
             type: 'popup',
             url: '/index.html'
         }); */
-    console.log(tab);
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: function() { window.close(); }
-    });
-});
+
+    // Prepare URL Filter
+    const url = vanillaURL.split('/');
+    console.log(url);
+
+    // URL Checker
+    /* if (
+        url.startsWith('ipfs://')
+    ) {
+
+        chrome.scripting.executeScript({
+            target: { tabId: tabID },
+            function: function() {
+                console.log('Close OLD NFT Window.');
+                window.close();
+            }
+        });
+
+    } */
+
+    // Complete
+    return;
+
+};
+
+// Icon Click
+chrome.action.onClicked.addListener(function(tab) { return openNFTPage(tab.id, tab.url, true); });
 
 // Show the demo page once the extension is installed
 /* chrome.runtime.onInstalled.addListener((_reason) => {
