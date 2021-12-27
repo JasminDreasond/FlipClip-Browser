cdae832324337332db858f74f3b8dea399453058
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-12-27 16:38:50 -0300

    fixed

--- background.js
+++ background.js
@@ -1,22 +1,13 @@
-/* Click on Extension Button */
-function reddenPage() {
-    document.body.style.backgroundColor = 'red';
-}
 chrome.action.onClicked.addListener(function(tab) {
     /*     chrome.windows.create({
             type: 'popup',
             url: '/index.html'
         }); */
+    console.log(tab);
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: reddenPage
+        function: function() { window.close(); }
     });
-});
-
-chrome.windows.onCreated.addListener({
-    callback: function(event) {
-        console.log(event);
-    }
 });
 
 // Show the demo page once the extension is installed
