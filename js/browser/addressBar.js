29fe1fa11ec7432e6cbdd7ab7673285bd63d2dd8
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-12-30 13:10:04 -0300

    browser base complete

--- js/browser/addressBar.js
+++ js/browser/addressBar.js
@@ -1,3 +1,16 @@
 var startAddressBar = function(fn) {
+
+    $('#browser').append(
+        $('<nav>', { class: 'navbar navbar-expand-lg navbar-light bg-light' }).append(
+            $('<ul>', { class: 'navbar-nav mr-auto' }).append(
+                $('<li>', { class: 'nav-item active' }).append(
+                    $('<a>', { class: 'nav-link', href: '#' }).text('Test')
+                )
+            )
+        )
+    );
+
+    // Complete
     fn();
+
 };
