e46b526544dc5bc272d6fa2efc558b97a979475d
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-12-30 16:16:00 -0300

    files update and resolution added

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # FlipClip-Browser
 Browser to browse NFT domains.
+
+```js
+var CIDTool = require('cid-tool');
+var unResolution = require('@unstoppabledomains/resolution');
+global.window.unResolution = unResolution;
+global.window.CIDTool = CIDTool;
+
+// browserify main.js -o bundle.js
+```
--- js/browser/browser.js
+++ js/browser/browser.js
@@ -1,3 +1,12 @@
 var startBrowser = function(fn) {
-    fn();
+
+    resolution.ipfsHash(params.domain).then((value) => {
+        console.log(CIDTool.base32(value));
+        resolution.addr(params.domain, 'BTC').then((value) => {
+            console.log(value);
+            fn();
+            return;
+        });
+        return;
+    });
 };
