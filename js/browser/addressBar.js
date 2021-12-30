3e3cd0550bb848fd703a59e177dd7b62e949cbed
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-12-30 19:23:47 -0300

    window fixed

--- js/browser/addressBar.js
+++ js/browser/addressBar.js
@@ -1,7 +1,7 @@
 var startAddressBar = function(fn) {
 
     $('#browser').append(
-        $('<nav>', { class: 'navbar navbar-expand-lg navbar-light bg-light' }).append(
+        $('<nav>', { class: 'navbar navbar-expand-lg navbar-light bg-light' }).css('height', browserSettings.addressBarSize).append(
             $('<ul>', { class: 'navbar-nav mr-auto' }).append(
                 $('<li>', { class: 'nav-item active' }).append(
                     $('<a>', { class: 'nav-link', href: '#' }).text('Test')
--- js/browser/browser.js
+++ js/browser/browser.js
@@ -1,8 +1,23 @@
+var browserSettings = { addressBarSize: 56 };
 var startBrowser = function(fn) {
 
     resolution.ipfsHash(params.domain).then((value) => {
-        console.log(CIDTool.base32(value));
+        const cid = CIDTool.base32(value);
         resolution.addr(params.domain, 'BTC').then((value) => {
+            $('#browser').append(
+                $('<iframe>', {
+                    src: 'https://' + cid + '.ipfs.dweb.link/',
+                    id: 'browser-window'
+                }).css({
+                    frameBorder: 0,
+                    'min-height': '100%',
+                    'min-width': '100%',
+                    height: '100%',
+                    width: '100%',
+                    position: 'fixed',
+                    'padding-bottom': browserSettings.addressBarSize
+                })
+            );
             console.log(value);
             fn();
             return;
