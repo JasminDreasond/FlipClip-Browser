daa40d26b36aa76e1c2d529b35bf21dbc753792a
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-12-30 19:42:58 -0300

    browser script update

--- js/browser/browser.js
+++ js/browser/browser.js
@@ -1,15 +1,25 @@
-var browserSettings = { addressBarSize: 56 };
+// Browser Scripts
+var browserSettings = {
+
+    // Address Bar
+    addressBarSize: 56,
+
+    // Proxy
+    proxy: 'https://{cid32}.ipfs.dweb.link/',
+    urlGenerator: function(value) { return browserSettings.proxy.replace('{cid}', value).replace('{cid32}', CIDTool.base32(value)); }
+
+};
+
 var startBrowser = function(fn) {
 
-    resolution.ipfsHash(params.domain).then((value) => {
-        const cid = CIDTool.base32(value);
+    resolution.ipfsHash(params.domain).then((cid) => {
         resolution.addr(params.domain, 'BTC').then((value) => {
             $('#browser').append(
                 $('<iframe>', {
-                    src: 'https://' + cid + '.ipfs.dweb.link/',
-                    id: 'browser-window'
+                    src: browserSettings.urlGenerator(cid),
+                    id: 'browser-window',
+                    frameBorder: 0
                 }).css({
-                    frameBorder: 0,
                     'min-height': '100%',
                     'min-width': '100%',
                     height: '100%',
