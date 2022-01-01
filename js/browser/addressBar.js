a47d319d505231da05c4ad11fceb2945fc7ea3ae
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-01-01 12:00:56 -0300

    address bar item

--- js/browser/addressBar.js
+++ js/browser/addressBar.js
@@ -6,58 +6,68 @@ browserSettings.updateAddressBar = function() {
 
 var startAddressBar = function(fn) {
 
-    $('#browser').append(
-        $('<div>', { id: 'address-bar' }).append(
-            $('<nav>', { class: 'navbar navbar-expand-lg navbar-' + browserSettings.theme + ' bg-' + browserSettings.theme, id: 'tabs' }).css('height', browserSettings.addressBar.size / 2 - 3).append(
-                $('<ul>', { class: 'navbar-nav mr-auto' }).append(
-                    $('<li>', { class: 'nav-item' }).append(
-                        $('<a>', { class: 'nav-link', href: '#' }).text('Test')
-                    )
+    // Prepare Nav
+    browserSettings.addressBar.nav = {};
+
+    // Tabs Item
+    browserSettings.addressBar.nav.tabs = $('<nav>', { class: 'navbar navbar-expand-lg navbar-' + browserSettings.theme + ' bg-' + browserSettings.theme, id: 'tabs' }).css('height', browserSettings.addressBar.size / 2 - 3).append(
+        $('<ul>', { class: 'navbar-nav mr-auto' }).append(
+            $('<li>', { class: 'nav-item' }).append(
+                $('<a>', { class: 'nav-link', href: '#' }).text('Test')
+            )
+        )
+    );
+
+    // Browser Items
+    browserSettings.addressBar.nav.items = $('<nav>', { class: 'navbar navbar-expand-lg navbar-' + browserSettings.theme + ' bg-' + browserSettings.theme, id: 'menu' }).css('height', browserSettings.addressBar.size / 2 + 3).append(
+        $('<ul>', { class: 'navbar-nav mr-auto' }).append(
+
+            $('<li>', { class: 'nav-item' }).append(
+                $('<a>', { class: 'nav-link mx-1', href: '#' }).append(
+                    $('<i>', { class: 'fas fa-caret-left' }).css({
+                        'font-size': browserSettings.addressBar.buttonSize,
+                        'margin': browserSettings.addressBar.marginButtonFix,
+                        'margin-right': 1
+                    })
                 )
             ),
-            $('<nav>', { class: 'navbar navbar-expand-lg navbar-' + browserSettings.theme + ' bg-' + browserSettings.theme, id: 'menu' }).css('height', browserSettings.addressBar.size / 2 + 3).append(
-                $('<ul>', { class: 'navbar-nav mr-auto' }).append(
 
-                    $('<li>', { class: 'nav-item' }).append(
-                        $('<a>', { class: 'nav-link mx-1', href: '#' }).append(
-                            $('<i>', { class: 'fas fa-caret-left' }).css({
-                                'font-size': browserSettings.addressBar.buttonSize,
-                                'margin': browserSettings.addressBar.marginButtonFix,
-                                'margin-right': 1
-                            })
-                        )
-                    ),
+            $('<li>', { class: 'nav-item' }).append(
+                $('<a>', { class: 'nav-link mx-1', href: '#' }).append(
+                    $('<i>', { class: 'fas fa-caret-right' }).css({
+                        'font-size': browserSettings.addressBar.buttonSize,
+                        'margin': browserSettings.addressBar.marginButtonFix,
+                        'margin-left': 1
+                    })
+                )
+            ),
 
-                    $('<li>', { class: 'nav-item' }).append(
-                        $('<a>', { class: 'nav-link mx-1', href: '#' }).append(
-                            $('<i>', { class: 'fas fa-caret-right' }).css({
-                                'font-size': browserSettings.addressBar.buttonSize,
-                                'margin': browserSettings.addressBar.marginButtonFix,
-                                'margin-left': 1
-                            })
-                        )
-                    ),
+            $('<li>', { class: 'nav-item' }).append(
+                $('<a>', { class: 'nav-link mx-1', href: '#' }).append(
+                    $('<i>', { class: 'fas fa-redo-alt' })
+                )
+            ),
 
-                    $('<li>', { class: 'nav-item' }).append(
-                        $('<a>', { class: 'nav-link mx-1', href: '#' }).append(
-                            $('<i>', { class: 'fas fa-redo-alt' })
-                        )
-                    ),
+            $('<li>', { class: 'nav-item' }).append(
+                $('<a>', { class: 'nav-link mx-1', href: '#' }).append(
+                    $('<i>', { class: 'fas fa-home' })
+                )
+            ),
 
-                    $('<li>', { class: 'nav-item' }).append(
-                        $('<a>', { class: 'nav-link mx-1', href: '#' }).append(
-                            $('<i>', { class: 'fas fa-home' })
-                        )
-                    ),
-
-                    $('<li>', { class: 'nav-item' }).append(
-                        $('<a>', { class: 'nav-link mx-1', href: '#' }).append(
-                            $('<i>', { class: 'fas fa-bookmark' })
-                        )
-                    )
-
+            $('<li>', { class: 'nav-item' }).append(
+                $('<a>', { class: 'nav-link mx-1', href: '#' }).append(
+                    $('<i>', { class: 'fas fa-bookmark' })
                 )
             )
+
+        )
+    );
+
+    // Set Browser Items
+    $('#browser').append(
+        $('<div>', { id: 'address-bar' }).append(
+            browserSettings.addressBar.nav.tabs.tabs,
+            browserSettings.addressBar.nav.tabs.items
         )
     );
 
