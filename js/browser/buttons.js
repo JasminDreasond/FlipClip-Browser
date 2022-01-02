8d90428034b6c10f6d11f43695991b710978ba4e
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-01-02 18:51:33 -0300

    Save Page 70% done

--- js/browser/buttons.js
+++ js/browser/buttons.js
@@ -22,7 +22,16 @@ browserSettings.buttons.settings = function() {
 
     // Get Data
     $('#settings .modal-body').empty();
-    chrome.storage.local.get([browserSettings.tabs[browserSettings.active].domain], async function(storage) {
+    const id = browserSettings.tabs[browserSettings.active].domain + '__privacy';
+    chrome.storage.local.get([id], async function(storage) {
+
+        // Create Storage
+        if (!storage[id]) {
+            storage[id] = {};
+        }
+
+        // Start Page        
+        $('#settings').modal('show');
 
         // Get Website Data
         const url = {
@@ -31,21 +40,31 @@ browserSettings.buttons.settings = function() {
             ) + '*'
         };
 
+        // Get Data
         const data = {};
         for (const item in settingsList) {
+
+            // Insert Default Data
             data[settingsList[item]] = await chrome.contentSettings[settingsList[item]].get(url);
+            if (!storage[id][settingsList[item]]) { storage[id][settingsList[item]] = data[settingsList[item]]; }
+
+            // Insert Cfg
+            $('#settings .modal-body').append(
+                $('<select>').change(function() {
+
+                    // Prepare Save
+                    chrome.storage.local.set(storage, function() {
+                        console.log('Value is set to ' + value);
+                    });
+
+                })
+            );
+
         }
 
-        console.log(data);
-        console.log(storage);
-
-        $('#settings .modal-body').empty().append(
-
-
-
-        );
-
-        $('#settings').modal('show');
+        // Prepare Save
+        await chrome.storage.local.set(storage);
+        return;
 
     });
 
