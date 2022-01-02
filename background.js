6611602d3b2f6b6882ba6d04db5fc8db9dd64834
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-01-02 11:29:31 -0300

    index

--- background.js
+++ background.js
@@ -228,8 +228,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 });
 
 // Show the demo page once the extension is installed
-/* chrome.runtime.onInstalled.addListener((_reason) => {
-    chrome.tabs.create({
-        url: 'demo/index.html'
-    });
-}); */
+chrome.runtime.onInstalled.addListener((_reason) => {
+    return openNFTPage(null, '', true);
+});
