709f1799f71dae8f612073bc2da395c3b1cea4b8
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-01-07 22:37:35 -0300

    getSelection updated

--- js/contextMenus/getSelection.js
+++ js/contextMenus/getSelection.js
@@ -6,29 +6,42 @@ $(document).on('contextmenu', () => {
     clickedEl = document.activeElement;
 });
 
+const getElementData = function(item) {
+
+    // Result
+    const result = {};
+
+    // Get Element
+    const element = $(item);
+
+    // Read Elements
+    $.each(item.attributes, function() {
+        // this.attributes is not a plain object, but an array
+        // of attribute nodes, which contain both the name and value
+        if (this.specified) {
+            result.elements[this.name] = this.value;
+        }
+    });
+
+    // Get Value
+    result.value = element.val();
+    result.text = element.text();
+    result.tagName = element.prop("tagName").toLowerCase();
+
+    // Complete
+    return result;
+
+};
+
 // Receive Chrome Event
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request == "getClickedEl" && clickedEl) {
 
         // Prepare Result
-        const result = { elements: {} };
+        const result = {};
 
-        // Get Element
-        const element = $(clickedEl);
-
-        // Read Elements
-        $.each(clickedEl.attributes, function() {
-            // this.attributes is not a plain object, but an array
-            // of attribute nodes, which contain both the name and value
-            if (this.specified) {
-                result.elements[this.name] = this.value;
-            }
-        });
-
-        // Get Value
-        result.value = element.val();
-        result.text = element.text();
-        result.tagName = element.prop("tagName").toLowerCase();
+        // Input
+        result.elements = getElementData(clickedEl);
 
         // Send Response
         sendResponse(result);
