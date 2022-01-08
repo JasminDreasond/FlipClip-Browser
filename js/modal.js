90a390849552ec697f718dddff49df4d57d5a2e7
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-01-08 09:16:14 -0300

    preparing modal data

--- js/modal.js
+++ js/modal.js
@@ -4,8 +4,21 @@ $(function() {
     const urlSearchParams = new URLSearchParams(window.location.search);
     var params = Object.fromEntries(urlSearchParams.entries());
 
+    // Insert Title
+    $('.modal-title').text(params.title);
+    document.title = params.title;
+
+    // Insert Data
+    $('.modal-body').text(params.message);
+
+    // Close Button
+    $('#close-modal, .close').click(function() { window.close(); });
+
+    // Open Modal
     $('#appstart').fadeOut(500, function() {
         $('#appstart').remove();
     });
+
+    $('#modal').fadeIn(500);
 
 });
