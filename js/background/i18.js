8f404024362c1636b0c67762a0f9d9de5a136f44
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-01-07 23:39:06 -0300

    i18 fixed

--- js/background/i18.js
+++ js/background/i18.js
@@ -1,7 +1,58 @@
-chrome.i18n.getMessage = function() {
-
-    return 'Mio';
-
+// Start Data
+var exLang = {
+    locale: chrome.i18n.getUILanguage(),
+    data: {}
 };
 
-startBackground();
+const addLanguage = function(lang) {
+    return new Promise(function(resolve, reject) {
+
+        try {
+
+            // Read Manifest
+            fetch(chrome.runtime.getURL('_locales/' + lang + '/messages.json'))
+                .then(response => {
+                    if (!response.ok) {
+                        reject(new Error("i18 HTTP error " + response.status));
+                    }
+                    return response.json();
+                })
+                .then(json => {
+                    for (const item in json) {
+                        if (typeof exLang.data[item] !== 'string') {
+                            exLang.data[item] = json[item].message;
+                        }
+                    }
+                    resolve();
+                })
+                .catch(reject);
+
+        } catch (err) {
+            reject(err);
+        }
+
+    });
+};
+
+// Read Manifest
+fetch(chrome.runtime.getURL('manifest.json'))
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("i18 HTTP error " + response.status);
+        }
+        return response.json();
+    })
+    .then(json => {
+        exLang.default_locale = json.default_locale;
+        addLanguage(exLang.locale).then(() => {
+
+        }).catch(() => {
+            addLanguage(exLang.default_locale).then(() => {
+                startBackground();
+            }).catch(err => { throw err; });
+        });
+    })
+    .catch(function(err) { throw err; });
+
+// Get Message
+chrome.i18n.getMessage = function(msgID) { return exLang.data[msgID]; };
