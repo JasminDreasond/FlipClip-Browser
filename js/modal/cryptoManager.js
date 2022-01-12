10c5048a3b8be1fe90d116c95c1c521bac3e7542
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-01-12 19:46:40 -0300

    preparing domain detector

--- js/modal/cryptoManager.js
+++ js/modal/cryptoManager.js
@@ -1,5 +1,38 @@
+var domains = {};
 var cryptoManager = function() {
+
+    // Module
+    var resolution = new unResolution.Resolution();
+
+    // Prepare Base
+    document.title = chrome.i18n.getMessage('cryptoManagerTitle');
+    $('.modal-title').text(chrome.i18n.getMessage('typeACrypto'));
     $('.modal-body').append(
         $('<input>', { type: 'text', class: 'form-control text-center' })
+
+        // Detect Domain
+        .change(function() {
+
+            // Domain DNS Selected
+            let dns = null;
+            const domain = $(this).val();
+            for (const where in domains) {
+                if (where !== 'filterGenerator') {
+                    for (const item in domains[where]) {
+                        if (domain.endsWith(domains[where][item])) {
+                            dns = where;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // Exist
+            if (dns) {;
+
+            }
+
+        })
     );
+
 };
